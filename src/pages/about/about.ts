d9a02a40a6b6864290c05eb9d6691c91c56dddd7
Oscar Bout <oscarbout@Ozzy-MAX.local>
2018-10-22 10:24:24 +0200

    assisngment 15

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 import { StorageProvider } from '../../providers/storage/storage'
+import { NavController, Tabs } from 'ionic-angular'
 
 @Component({
   selector: 'page-about',
@@ -8,15 +8,21 @@ import { StorageProvider } from '../../providers/storage/storage'
 })
 export class AboutPage {
 
+  tab: Tabs
+
   constructor(
     public navCtrl: NavController,
     private storage: StorageProvider
-    ) {
-
+  ) {
+    this.tab = this.navCtrl.parent;
   }
 
   public get fromHome() {
     return this.storage.getNumberFromHome
+  }
+
+  toHome() {
+    this.tab.select(0);
   }
 
 }
